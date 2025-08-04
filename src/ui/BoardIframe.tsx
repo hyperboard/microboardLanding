7e499f8aace6f002d89807ec61f8b047270d242b
Andrey Kameshkov <andreyka.kameshkov337@mail.ru>
2025-08-04 16:04:30 +0300

    refactor: landing iframe

--- src/ui/BoardIframe.tsx
+++ src/ui/BoardIframe.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 export const BoardIframe: React.FC<
 	React.HTMLAttributes<HTMLIFrameElement>
 > = () => {
 	return (
 		<iframe
 			className="mb-[128px] px-[8px] w-full min-h-[500px] h-[85dvh]"
-			src={`https://${window.location.origin.includes("dev-landing") ? "dev-app" : "app"}.microboard.io/welcome`}
+			src={`https://dev-app.microboard.io/welcome`}
 			allow="clipboard-read; clipboard-write"
 			sandbox="allow-forms allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-scripts allow-top-navigation allow-top-navigation-by-user-activation"
 			title="Microboard Welcome"
